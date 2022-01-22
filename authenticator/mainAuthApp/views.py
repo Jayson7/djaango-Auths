494c0cdcc7716cefe9c2b71f82df8b4f6452df09
jayson7 <adebayo.a@carrotsuite.xyz>
2022-01-22 09:58:06 +0100

    crispy forms config

--- authenticator/mainAuthApp/views.py
+++ authenticator/mainAuthApp/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms  import forms, UserCreationForm
 # Create your views here.
+
+
 def Homepage(request):
     
     return render(request, 'index.html')
 
-def register(request):
+
+def  register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_info = form.save(commit=False)
+            user_info.user = request.user 
+            user_info.save()
+            return redirect('login')
+        else:
+            print(form.errors)
+    else:
+        form = UserCreationForm()
+        
+
+    context = {'form' : form}
+
+    return render(request, 'registration/register.html', context)
+
     
-    return render(request, 'register.html')
